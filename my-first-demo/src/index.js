cf70df3662ab1f2913bc6ebefa47f05f2064d441
Taqui <mtaquis@gmail.com>
2023-07-06 10:40:25 +0530

    Props Drilling

--- my-first-demo/src/index.js
+++ my-first-demo/src/index.js
@@ -25,58 +25,64 @@ const books = [
 ];
 // console.log(books);
 function BookList() {
+  const someValue = "shakeAndBake";
+  const displayValue = () => {
+    console.log(someValue);
+  };
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} displayValue={displayValue} />;
       })}
     </section>
   );
 }
 
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e);
-    // console.log(e.target.name);
-  };
-
-  const handleButtonClick = () => {
-    alert("Handle button click");
-  };
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log("Form Submitted");
-  };
-  return (
-    <section>
-      <form onSubmit={handleFormSubmission}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          style={{ margin: "1rem 0" }}
-          onChange={handleFormInput}
-        />
-        <br></br>
-        <button onClick={() => console.log("Click mr")} type="button">
-          Click me
-        </button>
-      </form>
-    </section>
-  );
-};
-
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author, displayValue } = props;
   // console.log(props);
+
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayValue}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
+
+//LEARNING PART
+// const EventExamples = () => {
+//   const handleFormInput = (e) => {
+//     console.log(e);
+//     // console.log(e.target.name);
+//   };
+
+//   const handleButtonClick = () => {
+//     alert("Handle button click");
+//   };
+//   const handleFormSubmission = (e) => {
+//     e.preventDefault();
+//     console.log("Form Submitted");
+//   };
+//   return (
+//     <section>
+//       <form onSubmit={handleFormSubmission}>
+//         <h2>Typical Form</h2>
+//         <input
+//           type="text"
+//           name="example"
+//           style={{ margin: "1rem 0" }}
+//           onChange={handleFormInput}
+//         />
+//         <br></br>
+//         <button onClick={() => console.log("Click mr")} type="button">
+//           Click me
+//         </button>
+//       </form>
+//     </section>
+//   );
+// };
